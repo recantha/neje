0afc4f4edcb69d9f21de139cb825bf52300ef9d1
recantha <mike@recantha.co.uk>
2016-09-23 16:22:59 +0100

    Update engrave.sh

--- engrave.sh
+++ engrave.sh
@@ -7,17 +7,13 @@ echo Handshake?
 echo -e '\xF6' > $1
 sleep 1
 
-echo Reset
-echo -e '\xF9' > $1
-sleep 1
+rem echo Reset
+rem echo -e '\xF9' > $1
+rem sleep 1
 
-echo Sending to top-left corner and waiting for 5 seconds
-echo -e '\xF3' > $1
-sleep 5
-
-echo Sending to centre and waiting for 5 seconds
-echo -e '\xFB' > $1
-sleep 5
+rem echo Sending to centre and waiting for 5 seconds
+rem echo -e '\xFB' > $1
+rem sleep 5
 
 echo Initiate image transfer
 echo -e '\xFE\xFE\xFE\xFE\xFE\xFE\xFE\xFE' > $1
@@ -27,6 +23,10 @@ echo Send image
 cat $2 > $1
 echo Image sent
 sleep 3
+
+echo Sending to top-left corner and waiting for 5 seconds
+echo -e '\xF3' > $1
+sleep 5
 
 echo Set burning time
 echo -e '\x20' > $1
